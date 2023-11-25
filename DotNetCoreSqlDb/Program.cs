d984a7a1c9cc88b1b7b27910cdb4c0e840b8ac24
Michael Polivka <michael.polivka1@gmail.com>
2023-11-25 17:46:59 +0100

    fixing

--- DotNetCoreSqlDb/Program.cs
+++ DotNetCoreSqlDb/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add database context and cache
 builder.Services.AddDbContext<MyDatabaseContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")))
+    options.UseNpgsql(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
